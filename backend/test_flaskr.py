861ba588a7b13dac903330f5b6fceb57afc80116
Jaouad Eddadsi <jaouad.eddadsi@ensta-bretagne.org>
2020-04-21 17:14:13 +0000

    Create GET questions endpoint

---
+++ backend/test_flaskr.py
@@ -0,0 +1,54 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path = "postgres:///{}".format(self.database_name)
+        setup_db(self.app, self.database_path)
+
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    """
+    TODO
+    Write at least one test for each test for successful operation and for expected errors.
+    """
+
+    def test_get_questions(self):
+        res = self.client().get("/questions?page=1")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["questions"])
+        self.assertEqual(len(data["questions"]), 10)
+        self.assertEqual(data["total_questions"], 19)
+        self.assertTrue(data["current_categories"])
+        self.assertEqual(len(data["current_categories"]), 4)
+        self.assertTrue(data["categories"])
+        self.assertEqual(len(data["categories"]), 6)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
